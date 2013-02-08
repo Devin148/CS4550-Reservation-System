ede04ef167c67872c0eb8a309c81b8a80066808b
Devin148 <devinhurd@gmail.com>
2013-02-08 17:57:22 -0500

    Add basic form validation

--- form.php
+++ form.php
@@ -6,21 +6,21 @@ http://www.phpform.org/
 -->
 
 <div id="form_container">
-    <form  method="post" action="">
+    <form name="reservation" method="post" onsubmit="return validateForm();" action="reserve.php">
         <ul>
             <li>
                 <label class="description">Name</label>
                 <span>
-                    <input id="first" type="text" maxlength="255" size="7" value="" />
+                    <input id="first" name="first" type="text" maxlength="255" size="7" value="" />
                     <label for="first">First</label>
                 </span>
                 <span>
-                    <input id="last" type="text" maxlength="255" size="12" value="" />
+                    <input id="last" name="last" type="text" maxlength="255" size="12" value="" />
                     <label for="last">Last</label>
                 </span> 
             </li>
             <li>
-                <label class="description" for="email">Email</label>
+                <label class="description" name="email" for="email">Email</label>
                 <div>
                     <input id="email" type="text" maxlength="255" value="" /> 
                 </div> 
@@ -28,34 +28,34 @@ http://www.phpform.org/
             <li>
                 <label class="description">Date</label>
                 <span>
-                    <input id="month" size="1" maxlength="2" value="" type="text" /> /
+                    <input id="month" name="month" size="1" maxlength="2" value="" type="text" /> /
                     <label for="month">MM</label>
                 </span>
                 <span>
-                    <input id="day" size="1" maxlength="2" value="" type="text" /> /
+                    <input id="day" name="day" size="1" maxlength="2" value="" type="text" /> /
                     <label for="day">DD</label>
                 </span>
                 <span>
-                    <input id="year" size="4" maxlength="4" value="" type="text" />
+                    <input id="year" name="year" size="4" maxlength="4" value="" type="text" />
                     <label for="year">YYYY</label>
                 </span>
             </li>
             <li>
                 <label class="description">Time</label>
                 <span>
-                    <input id="hour" size="2" type="text" maxlength="2" value="" /> : 
+                    <input id="hour" name="hour" size="2" type="text" maxlength="2" value="" /> : 
                     <label for="hour">HH</label>
                 </span>
                 <span>
-                    <input id="minute" size="2" type="text" maxlength="2" value="" /> : 
+                    <input id="minute" name="minute" size="2" type="text" maxlength="2" value="" /> : 
                     <label for="minute">MM</label>
                 </span>
                 <span>
-                    <input id="second" size="2" type="text" maxlength="2" value="" />
+                    <input id="second" name="second" size="2" type="text" maxlength="2" value="" />
                     <label for="second">SS</label>
                 </span>
                 <span>
-                    <select style="width:4em" id="element_3_4">
+                    <select style="width:4em" id="tod" name="tod">
                         <option value="AM" >AM</option>
                         <option value="PM" >PM</option>
                     </select>
@@ -65,7 +65,7 @@ http://www.phpform.org/
             <li>
                 <label class="description" for="guests">Number of Guests</label>
                 <div>
-                    <input id="guests" type="text" maxlength="255" value="" /> 
+                    <input id="guests" name="guests" type="text" maxlength="255" value="" /> 
                 </div> 
             </li>
             <li>
@@ -74,3 +74,40 @@ http://www.phpform.org/
         </ul>
     </form>
 </div>
+
+<script>
+function validateForm() {
+    var first = document.forms["reservation"]["first"].value;
+    var last = document.forms["reservation"]["last"].value;
+
+    var email = document.forms["reservation"]["email"].value;
+
+    var month = document.forms["reservation"]["month"].value;
+    var day = document.forms["reservation"]["day"].value;
+    var year = document.forms["reservation"]["year"].value;
+
+    var hour = document.forms["reservation"]["hour"].value;
+    var minute = document.forms["reservation"]["minute"].value;
+    var second = document.forms["reservation"]["second"].value;
+
+    var tod = document.forms["reservation"]["tod"].value;
+
+    var guests = document.forms["reservation"]["guests"].value;
+
+    if (isEmpty(first) || isEmpty(last) ||
+        isEmpty(email) ||
+        isEmpty(month) || isEmpty(day) || isEmpty(year) ||
+        isEmpty(hour) || isEmpty(minute) || isEmpty(second) ||
+        isEmpty(tod) || isEmpty(guests)) {
+        alert("One of the fields has been left empty");
+        return false;
+    }
+}
+
+function isEmpty(field) {
+    if (field == null || field =="") {
+        return true;
+    }
+    return false;
+}
+</script>
---
+++ reserve.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Reservation</title>
+    <link href="stylesheets/index.css" rel="stylesheet" type="text/css" />
+    <link href="stylesheets/navbar.css" rel="stylesheet" type="text/css" />
+    <link href="stylesheets/view.css" rel="stylesheet" type="text/css" media="all">
+    <script src="js/jquery-1.9.0.min.js" type="text/javascript"></script>
+    <script src="js/slideshow.js" type="text/javascript"></script>
+    <script src="js/view.js" type="text/javascript"></script>
+    <script src="js/calendar.js" type="text/javascript"></script>
+</head>
+
+<body>
+
+    <?php
+    $current="";
+    include("navbar.php");
+    ?>
+
+    <div id="content">
+        <div id="content_left">
+            <?php
+            $first = $_POST["first"];
+            ?>
+            <h1>Thank you, <?=$first?>, for making a reservation!</h1>
+            <h2>We will contact you within 24 hours about your reservation.</h2>
+        </div>
+        <div id="content_right">
+            <p> Right Content </p>
+        </div>
+    </div>
+</body>
+</html>
