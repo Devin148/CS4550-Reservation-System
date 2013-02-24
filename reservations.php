9d8bab79cddb4f5c7d81cbb0255a7ac6d1d62ebc
Devin148 <devinhurd@gmail.com>
2013-02-24 16:53:48 -0500

    Add reservation lookup

--- reservations.php
+++ reservations.php
@@ -8,7 +8,6 @@
     <link href="stylesheets/view.css" rel="stylesheet" type="text/css" media="all">
     <script src="js/jquery-1.9.0.min.js" type="text/javascript"></script>
     <script src="js/slideshow.js" type="text/javascript"></script>
-    <script src="js/form.js" type="text/javascript"></script>
 </head>
 
 <body>
@@ -21,19 +20,80 @@
     <div id="content">
         <div id="content_left">
             <h1>Reservations</h1>
-            <h3>Reservation lookup</h2>
-            <form name="lookup" method="post" onsubmit="return validateEmail(document.forms["lookup"]["email"].value);" action="lookup.php">
-                <label for="email">Email:</label>
+            <?php
+            if (isset($_POST["email"])) {
+            ?>
+
+            <h3>Your reservations</h3>
+
+            <?php
+                getReservations($_POST["email"]);
+            } else {
+            ?>
+
+            <h3>Reservation lookup</h3>
+            <form name="lookup" method="post" onsubmit="return validateEmail();" action="reservations.php">
+                <label id="email" class="description" name="email" for="email">Email</label>
                 <input id="email" name="email" type="text" maxlength="255" size="15" value="" />
                 <input id="submit" class="button_text" type="submit" value="Submit" />
             </form>
 
-            <h3>Make a reservation</h2>
+            <?php
+            }
+            ?>
         </div>
         <div id="content_right">
             <h2>Twitter @Club_Swimmfrog</h2>
             <p>Tweet</p>
         </div>
     </div>
+
+    <?php
+
+    function getReservations($email) {
+        $mysqli = new mysqli("swimmfrogcom.ipagemysql.com","res_user","res_pass");
+        if ($mysqli->connect_errno) {
+            echo("failed to connect to db: " . $mysqli->connect_error);
+            return false;
+        }
+
+        $mysqli->select_db("swimmfrog_res");
+
+        $stmt = $mysqli->prepare("SELECT name, res_date, guests FROM reservations WHERE email=?");
+        $stmt->bind_param('s', $email);
+        $stmt->execute();
+        $stmt->bind_result($name, $mysqldate, $guests);
+        echo ("<table>");
+        echo("<tr><td>Name</td><td>Date & Time</td><td>Number of Guests</td></tr>");
+        while ($stmt->fetch()) {
+            echo("<tr>");
+            echo("<td>" . $name . "</td>");
+            echo("<td>" . $mysqldate . "</td>");
+            echo("<td>" . $guests . "</td>");
+            echo("</tr>");
+        }
+        echo ("</table>");
+
+        return true;
+    }
+
+    ?>
+
+    <script>
+    function validateEmail() {
+        // Get the email
+        var email = document.forms["lookup"]["email"].value
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        
+        // Check if it's a real email
+        if (re.test(email)) {
+            return true;
+        } else {
+            $("label#email").css("color", "#F00");
+            $("label#email").html("* Please enter a valid email");
+            return false;   
+        }
+    }
+    </script>
 </body>
 </html>
