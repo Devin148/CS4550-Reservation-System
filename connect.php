76187f3bd93f642997412c72ddcccbcab9a9c3a6
devin148 <devinhurd@gmail.com>
2013-04-10 14:10:25 -0400

    Add comments

--- connect.php
+++ connect.php
@@ -1,11 +1,9 @@
 <?php
-
-require_once("config.php");
-
+// Example connection
 $mysqli = new mysqli("localhost", "local_user", "local_pass");
 if ($mysqli->connect_errno) {
     print "Failed to connect to db: " . $mysqli->connect_error;
     exit();
 }
-
+// Example database
 $mysqli->select_db("my_db");
